e925f86777fac016bebeff3971e84fd76c99e0d5
GenTToX <142807717+GenTToX@users.noreply.github.com>
2023-09-11 10:57:13 +0200

    Update Kanders.sql

--- Kanders.sql
+++ Kanders.sql
@@ -183,10 +183,10 @@ CREATE TABLE `Bundesland`(
 --
 
 CREATE TABLE `Band` (
-  `Band_ID` int NOT NULL AUTO_INCREMENT,
+  `BAND_ID` int NOT NULL AUTO_INCREMENT,
   `Name` varchar(40) NOT NULL,
 
-  PRIMARY KEY (`Band_ID`)
+  PRIMARY KEY (`BAND_ID`)
 );
 
 INSERT INTO `Band` (`Name`) VALUES
@@ -226,10 +226,10 @@ CREATE TABLE `Musiker` (
 --
 
 CREATE TABLE `Song` (
-  `Song_ID` int NOT NULL AUTO_INCREMENT,
+  `SONG_ID` int NOT NULL AUTO_INCREMENT,
   `Song_Typ` varchar(40),
   `Name` varchar(40),
-  PRIMARY KEY (`Song_ID`)
+  PRIMARY KEY (`SONG_ID`)
 );
 
 --
@@ -240,11 +240,234 @@ CREATE TABLE `Musik-Richtung`(
     `MURI_ID` int NOT NULL AUTO_INCREMENT,
     `Bezeichnung` varchar(40) DEFAULT NULL,
 );
+-- --------------------------------------------------------------------------------------------
+
+CREATE TABLE `R_Mitglied_besitzt_eine_Rolle`(
+`MITG_ID` int NOT NULL,
+`ROLL_ID` int NOT NULL,
+);
 
 
+
+
+
+CREATE TABLE `R_Veranstaltung_wird_vorgeschlagen_von_Mitglied`(
+`VERA_ID` int NOT NULL,
+`MITG_ID` int NOT NULL,
+);
+
+
+
+
+CREATE TABLE `R_Veranstaltung_findet_statt_in_Bundesland`(
+`VERA_ID` int NOT NULL,
+`BULA_ID` int NOT NULL,
+);
+
+
+
+
+CREATE TABLE `R_Veranstaltung_findet_statt_in_Region`(
+`VERA_ID` int NOT NULL,
+`REGI_ID` int NOT NULL,
+);
+
+
+
+
+CREATE TABLE `R_Veranstaltung_findet_statt_in_Stadtteil`(
+`VERA_ID` int NOT NULL,
+`STTL_ID` int NOT NULL,
+);
+
+
+
+
+CREATE TABLE `R_Veranstaltung_findet_statt_in_Stadt`(
+`VERA_ID` int NOT NULL,
+`STDT_ID` int NOT NULL,
+);
+
+
+
+
+CREATE TABLE `R_Veranstaltung_findet_statt_in_Veranstaltungsort`(
+`VERA_ID` int NOT NULL,
+`VAOT_ID` int NOT NULL,
+);
+
+
+
+
+CREATE TABLE `R_Veranstaltung_hat_Preis-Typ`(
+`VERA_ID` int NOT NULL,
+`PRTP_ID` int NOT NULL,
+);
+
+
+
+CREATE `R_Veranstaltung_hat_Veranstaltungs-Typ`(
+  `VERA_ID` int NOT NULL,
+  `VETP_ID` int NOT NULL,
+)
+
+
+
+CREATE TABLE `R_Band_ist_Haupt/Vorgruppe_bei_Konzert`(
+`VERA_ID` int NOT NULL,
+`BAND_ID` int NOT NULL,
+);
+
+
+
+
+
+CREATE TABLE `R_Band_tritt_auf_bei_Festival`(
+`VERA_ID` int NOT NULL,
+`BAND_ID` int NOT NULL,
+`Datum` date NOT NULL,
+`Uhrzeit` time NOT NULL,
+`Wochentag` varchar(40) DEFAULT NULL,
+);
+
+
+
+
+CREATE TABLE `R_Band_spielt_Musik-Richtung`(
+`BAND_ID` int NOT NULL,
+`Muri_ID` int NOT NULL,
+);
+
+
+
+
+CREATE TABLE `R_Band_hat_Song`(
+`BAND_ID` int NOT NULL,
+`SONG_ID` int NOT NULL,
+);
+
+
+
+
+CREATE TABLE `R_Musiker_spielt_in_Band`(
+`BAND_ID` int NOT NULL,
+`MUSI_ID` int NOT NULL,
+);
 
 
 
 -- --------------------------------------------------------------------------------------------
+
+ALTER TABLE R_spielt_in_Musiker
+ADD FOREIGN KEY (BAND_ID) REFERENCES Band(BAND_ID);
+
+ALTER TABLE R_spielt_in_Musiker
+ADD FOREIGN KEY (MUSI_ID) REFERENCES Musiker(MUSI_ID);
+-- -----------
+
+ALTER TABLE R_Band_hat_Song
+ADD FOREIGN KEY (BAND_ID) REFERENCES Band(BAND_ID);
+
+ALTER TABLE R_Band_hat_Song
+ADD FOREIGN KEY (MUSI_ID) REFERENCES Musiker(MUSI_ID);
+
+-- ----------
+
+ALTER TABLE R_Band_spielt_Musik-Richtung
+ADD FOREIGN KEY(BAND_ID) REFERENCES Band(BAND_ID);
+
+ALTER TABLE R_Band_spielt_Musik-Richtung
+ADD FOREIGN KEY (Muri_ID) REFERENCES Musiker(MUSI_ID);
+
+-- ---------
+
+ALTER TABLE R_Band_tritt_auf_bei_Festival
+ADD FOREIGN KEY(VERA_ID) REFERENCES Festival(VERA_ID);
+
+ALTER TABLE R_Band_tritt_auf_bei_Festival
+ADD FOREIGN KEY(BAND_ID) REFERENCES Band(BAND_ID);
+
+-- ---------
+
+ALTER TABLE R_Band_ist_Haupt/Vorgruppe_bei_Konzert
+ADD FOREIGN KEY(BAND_ID) REFERENCES Band(BAND_ID);
+
+ALTER TABLE R_Band_ist_Haupt/Vorgruppe_bei_Konzert
+ADD FOREIGN KEY(VERA_ID) REFERENCES Konzert(VERA_ID);
+
+-- --------
+
+ALTER TABLE R_Veranstaltung_hat_Preis-Typ
+ADD FOREIGN KEY (VERA_IDD) REFERENCES Veranstaltung(VERA_ID);
+
+ALTER TABLE R_Veranstaltung_hat_Preis-Typ
+ADD FOREIGN KEY(PRTP_ID) REFERENCES Preis-Typ(PRTP_ID);
+
+-- --------
+
+ALTER TABLE R_Veranstaltung_hat_Veranstaltungs-Typ
+ADD FOREIGN KEY(VERA_ID) REFERENCES Veranstaltung(VERA_ID);
+
+ALTER TABLE R_Veranstaltung_hat_Veranstaltungs-Typ
+ADD FOREIGN KEY(VETP_ID) REFERENCES Veranstaltungs-Typ(VETP_ID);
+
+-- --------
+ALTER TABLE R_Veranstaltung_findet_statt_in_Veranstaltungsort
+ADD FOREIGN KEY(VERA_ID) REFERENCES Veranstaltung(VERA_ID);
+
+ALTER TABLE R_Veranstaltung_findet_statt_in_Veranstaltungsort
+ADD FOREIGN KEY(VAOT_ID) REFERENCES Veranstaltungsort(VAOT_ID);
+
+-- -------
+
+ALTER TABLE R_Veranstaltung_findet_statt_in_Stadt
+ADD FOREIGN KEY(VERA_ID) REFERENCES Veranstaltung(VERA_ID);
+
+ALTER TABLE R_Veranstaltung_findet_statt_in_Stadt
+ADD FOREIGN KEY(STDT_ID) REFERENCES Stadt(STDT_ID);
+
+-- -----
+
+ALTER TABLE R_Veranstaltung_findet_statt_in_Stadtteil
+ADD FOREIGN KEY(VER_ID) REFERENCES Veranstaltung(VERA_ID);
+
+ALTER TABLE R_Veranstaltung_findet_statt_in_Stadtteil
+ADD FOREIGN KEY(STTL_ID) REFERENCES Stadtteil(STTL_ID);
+
+-- -----
+
+ALTER TABLE R_Veranstaltung_findet_statt_in_Region
+ADD FOREIGN KEY(VERA_ID) REFERENCES Veranstaltung(VERA_ID);
+
+ALTER TABLE R_Veranstaltung_findet_statt_in_Region
+ADD FOREIGN KEY(REGI_ID) REFERENCES Region(REGI_ID);
+
+-- -----
+
+ALTER TABLE R_Veranstaltung_findet_statt_in_Bundesland
+ADD FOREIGN KEY(VERA_ID) REFERENCES Veranstaltung(VERA_ID);
+
+ALTER TABLE R_Veranstaltung_findet_statt_in_Bundesland
+ADD FOREIGN KEY(BULA_ID) REFERENCES Bundesland(BULA_ID);
+
+-- -------
+
+ALTER TABLE R_Veranstaltung_wird_vorgeschlagen_von_Mitglied
+ADD FOREIGN KEY(VER_ID) REFERENCES Veranstaltung(VERA_ID)
+
+ALTER TABLE R_Veranstaltung_wird_vorgeschlagen_von_Mitglied
+ADD FOREIGN KEY(MITG_ID) REFERENCES Mitglied(MITG_ID);
+
+-- -------
+
+ALTER TABLE R_Mitglied_besitzt_eine_Rolle
+ADD FOREIGN KEY(MITG_ID) REFERENCES Mitglied(MITG_ID);
+
+ALTER TABLE R_Mitglied_besitzt_eine_Rolle
+ADD FOREIGN KEY(ROLL_ID) REFERENCES Rolle(ROLL_ID);
+
+-- --------------------------------------------------------------------------------------------
 ALTER TABLE`Mitglied`
  ADD FOREIGN KEY (ROLL_ID) REFERENCES Rolle(ROLL_ID);
+
+ 
