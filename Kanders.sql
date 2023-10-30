863473fb5a4c6cd03db9bd747d79034a7ba2c0be
Maxinum916 <142808725+Maxinum916@users.noreply.github.com>
2023-10-30 20:48:09 +0100

    Update Kanders.sql

--- Kanders.sql
+++ Kanders.sql
@@ -364,11 +364,7 @@ INSERT INTO `Musik_Richtung`(`Bezeichnung`, `BAND_ID`) VALUES
 --
 
 CREATE TABLE `Gueltieger_Ort`(
-    `GORT_ID` int NOT NULL AUTO_INCREMENT,
-    `Stadt` varchar(40) DEFAULT NULL,
-    `Stadtteil` varchar(40) DEFAULT NULL,
-    `Region` varchar(40) DEFAULT NULL,
-    `Bundesland` varchar(40) DEFAULT NULL,
+    `GORT_ID` int NOT NULL AUTO_INCREMENT
 
     PRIMARY KEY(`GORT_ID`),
 
@@ -381,7 +377,8 @@ CREATE TABLE `Gueltieger_Ort`(
 );
 
 
-
+INSERT INTO `Gueltieger_Ort`(`STDT_ID`, `STTL_ID`, `REGI_ID`, `BULA_ID`) VALUES
+(1, , , );
 
 
 
